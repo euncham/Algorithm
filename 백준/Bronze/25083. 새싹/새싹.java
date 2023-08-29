5e254103334116abbe697575598a208fd9ace349
euncham <139742134+euncham@users.noreply.github.com>
2023-08-29 18:03:28 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13920 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,12 +1,10 @@
 public class Main {
-	public static void main(String[] args) {
-		String s = "         ,r'\"7\n" + 	
-				   "r`-_   ,'  ,/\n" + 		
-				   " \\. \". L_r'\n" + 		
-				   "   `~\\/\n" + 			
-				   "      |\n" + 			
-				   "      |";
- 
-		System.out.print(s);
-	}
+    public static void main(String[] args) {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
 }
